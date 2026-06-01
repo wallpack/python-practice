68c8234cb815352c294a7c0c703204e6e4e83d85
noctxbt <noctxbt@gmail.com>
2026-06-01 18:49:09 +0500

    str_min_and_max exercise

---
+++ str_min_and_max.py
@@ -0,0 +1,15 @@
+s = input()
+
+min_str = s
+max_str = s
+
+while s != 'КОНЕЦ':
+    if s < min_str:
+        min_str = s
+    if s > max_str:
+        max_str = s
+
+    s = input()
+
+print(f'Минимальная строка ⬇️: {min_str}')
+print(f'Максимальная строка ⬆️: {max_str}')
